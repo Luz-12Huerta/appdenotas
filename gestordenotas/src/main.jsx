62c078bd6c57ccffaac7622c284cc609a582b9bc
Luz-12Huerta <luz.huerta@capgemini.com>
2025-11-13 18:53:33 -0600

    Documentos Base

---
+++ gestordenotas/src/main.jsx
@@ -0,0 +1,13 @@
+// src/main.jsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./index.css";
+import App from "./App.jsx";//componente principal de la aplicación donde se configuran las rutas
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
